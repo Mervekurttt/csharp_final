6ced3923b05ff3e670c4df95435e3097fae7d2dc
Mervekurttt <96375907+Mervekurttt@users.noreply.github.com>
2022-01-25 22:36:36 +0300

    Add files via upload

---
+++ csharpMerve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace WindowsFormApplisation2
+{
+    public partial class Form1 : Form
+    {
+        public Form1();
+
+        {
+            InitializeComponent();
+        }
+           private void=
+        {
+            baglanti Open;
+        
+            baglanti.Close();
+        }
+    baglanti.Close();
+    }
+}
+
+namespace csharpinalmerve
+{
+    
+    }
+}
